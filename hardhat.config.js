ac32f3febc9c77975e35a88832787b220c2a81fb
Matt Hamilton <matt@dharach.com>
2023-06-05 10:21:45 -0400

    fix chain id

--- hardhat.config.js
+++ hardhat.config.js
@@ -25,7 +25,7 @@ module.exports = {
             accounts: [PRIVATE_KEY],
         },
         calibration: {
-            chainId: 31415926,
+            chainId: 314159,
             url: "https://api.calibration.node.glif.io/rpc/v1",
             accounts: [PRIVATE_KEY],
         },
